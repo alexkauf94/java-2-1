aefa3c6b813b4701c00155e750756827216a9839
Alex Kaufman <alexkauf94@gmail.com>
2023-04-27 21:24:13 +0700

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int rubleForMile = 20;
+        int priceTicket = 13676;
+
+        int mile = priceTicket / rubleForMile;
+
+        System.out.println(mile);
+    }
+}
